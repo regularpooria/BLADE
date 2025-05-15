3321d42f9268ed7257f814484abc0a6e1c09a773
pooriaahmadi <pooriababak444@gmail.com>
2025-05-14 21:44:26 -0400

    added AgentRun's HTTP version

--- setup_code_execution.sh
+++ setup_code_execution.sh
@@ -1,4 +1,5 @@
-git clone https://github.com/Jonathan-Adly/agentrun
-cd agentrun/agentrun-api
+git submodule init
+git submodule update
+cd AgentRun/agentrun-api
 cp .env.example .env.dev
 docker-compose up -d --build
